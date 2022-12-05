f4c0e7bb301de440db51058061ce646d21001e63
asp977 <akashpagi07@gmail.com>
2022-12-05 23:38:01 +0530

    Task 1 & For loop questions

---
+++ T1_solutions.py
@@ -0,0 +1,12 @@
+import math
+# Q1 :
+print('Data','Science','Membership','Program','By','CampusX',sep = '-', end='')
+
+print()
+# Q2:
+# converting C to F
+celsius_num = int(input('Enter the Temperature in Celsius :'))
+
+result = (celsius_num * 1.8) + 32
+
+print('Fahrenheit is ', result)
